9c83060f9fa29ed57d83562c69b3a8acbb73cbc7
Will <wtu20@uclive.ac.nz>
2024-06-01 17:37:26 +1200

    feat: initialised starting positions and removed test code

--- src/main/java/com/willturner/chess/Board.java
+++ src/main/java/com/willturner/chess/Board.java
@@ -12,11 +12,23 @@ public class Board {
             pieceLocations[i][1] = new Pawn(PieceColour.BLACK, "♟");
             pieceLocations[i][6] = new Pawn(PieceColour.WHITE, "♙");
         }
-        pieceLocations[2][3] = new Bishop(PieceColour.WHITE, "♗");
-        pieceLocations[2][4] = new Rook(PieceColour.BLACK, "♜");
-        pieceLocations[3][4] = new Queen(PieceColour.WHITE, "♕");
-        pieceLocations[3][5] = new Knight(PieceColour.BLACK, "♞");
-        pieceLocations[4][5] = new Pawn(PieceColour.WHITE, "♙");
+        // initialise black  pieces
+        pieceLocations[0][0] = new Rook(PieceColour.BLACK, "♜");
+        pieceLocations[7][0] = new Rook(PieceColour.BLACK, "♜");
+        pieceLocations[1][0] = new Knight(PieceColour.BLACK, "♞");
+        pieceLocations[6][0] = new Knight(PieceColour.BLACK, "♞");
+        pieceLocations[2][0] = new Bishop(PieceColour.BLACK, "♝");
+        pieceLocations[5][0] = new Bishop(PieceColour.BLACK, "♝");
+        pieceLocations[3][0] = new Queen(PieceColour.BLACK, "♛");
+
+        // initialise white pieces
+        pieceLocations[0][7] = new Rook(PieceColour.WHITE, "♖");
+        pieceLocations[7][7] = new Rook(PieceColour.WHITE, "♖");
+        pieceLocations[1][7] = new Knight(PieceColour.WHITE, "♘");
+        pieceLocations[6][7] = new Knight(PieceColour.WHITE, "♘");
+        pieceLocations[2][7] = new Bishop(PieceColour.WHITE, "♗");
+        pieceLocations[5][7] = new Bishop(PieceColour.WHITE, "♗");
+        pieceLocations[3][7] = new Queen(PieceColour.WHITE, "♕");
     }
     public ChessPiece getPiece(Location location){
         int column = location.getColumn();
